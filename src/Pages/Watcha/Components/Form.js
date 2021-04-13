d3daf4dadbfe5115afca4438902284dfa43131fe
오종택 <ohjtack@gmail.com>
2021-04-14 00:32:35 +0900

    modify: disabled validation logic

--- src/Pages/Watcha/Components/Form.js
+++ src/Pages/Watcha/Components/Form.js
@@ -5,16 +5,16 @@ import Input from "./Input";
 import "./Form.scss";
 
 export default class Form extends Component {
-  state = {
-    name: "",
-    email: "",
-    password: "",
-  }
+  // state = {
+  //   name: "",
+  //   email: "",
+  //   password: "",
+  // }
 
-  handleInput = (e) => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value })
-  }
+  // handleInput = (e) => {
+  //   const { name, value } = e.target;
+  //   this.setState({ [name]: value })
+  // }
 
   render() {
     const { type, text, data } = this.props;
@@ -28,9 +28,9 @@ export default class Form extends Component {
               key={idx}
               type={input.type}
               text={input.text}
-              value={this.state[input.type]}
-              handleInput={this.handleInput}
-              handleValid={validator[input.type]}
+              // value={this.state[input.type]}
+              // handleInput={this.handleInput}
+              // handleValid={validator[input.type]}
             />
           ))}
         </div>
@@ -45,8 +45,8 @@ export default class Form extends Component {
   }
 }
 
-const validator = {
-  name: (input) => input.length >= 6,
-  email: (input) => input?.includes("@" && ".com"),
-  password: (input) => input.length >= 8,
-};
+// const validator = {
+//   name: (input) => input.length >= 6,
+//   email: (input) => input?.includes("@" && ".com"),
+//   password: (input) => input.length >= 8,
+// };
