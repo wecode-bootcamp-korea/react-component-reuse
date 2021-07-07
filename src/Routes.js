8ae7801b335fdad5e2daf5e0593cf0007836a0ec
Yeonuk Hwang <younuk23@gmail.com>
2021-07-07 13:25:20 +0900

    Add: ChildrenTest Component add

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Kakao from "./Pages/Kakao/CardList";
 import Watcha from "./Pages/Watcha/Modal";
+import ChildrenTest from "./Pages/Watcha/Components/ChildrenTest";
 
 const Examples = () => (
   <Router>
     <Switch>
       <Route exact path="/kakao" component={Kakao} />
       <Route path="/watcha" component={Watcha} />
+      <Route path="/children" component={ChildrenTest} />
     </Switch>
   </Router>
 );
