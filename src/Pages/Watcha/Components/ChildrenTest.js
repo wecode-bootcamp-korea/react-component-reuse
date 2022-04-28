1cd0c86c11eee8a040aaee6b36eff33b40077d5f
sstaar91 <sstaar91@gmail.com>
2022-04-28 17:26:47 +0900

    MODIFY : delete annotation

--- src/Pages/Watcha/Components/ChildrenTest.js
+++ src/Pages/Watcha/Components/ChildrenTest.js
@@ -2,10 +2,7 @@ import React from "react";
 import Button from "./Button";
 
 function ChildrenTest() {
-  return (
-    // <Button>Hello 27th</Button>
-    <Button value="Hello 27th" />
-  );
+  return <Button value="Hello th" />;
 }
 
 export default ChildrenTest;
